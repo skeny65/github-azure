2d593e9438e751a497efcac79a886b5860fd1e23
Keny <keny.bastidas@gmail.com>
2026-03-09 18:53:39 -0500

    update container

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -3,7 +3,7 @@ terraform {
   backend "azurerm" {
     resource_group_name  = "rg-terraform-state"
     storage_account_name = "gitaztfstatedevops2026"
-    container_name       = "tfstate"
+    container_name       = "tfstatekeny"
     key                  = "terraform.tfstate"
   }
 }
