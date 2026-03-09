219568fd30caacb10c90ac864a22e0b2c66e2b58
Keny <keny.bastidas@gmail.com>
2026-03-09 18:57:07 -0500

    update provider

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,11 +1,12 @@
 terraform {
-    required_providers {
-        azurerm = {
-            source = "hashicorp/azurerm"
-            version = "~> 4.63.0"
-        }
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.63.0"
     }
+  }
 }
 
 provider "azurerm" {
-    features {
+  features {}
+}
